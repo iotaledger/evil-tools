ca91bc56c273b0a97b04fbf92ad62382f903d826
Daria Dziubałtowska <daria305@gmail.com>
2023-11-29 13:29:34 +0100

    Update to latest iotago

--- pkg/accountwallet/faucet.go
+++ pkg/accountwallet/faucet.go
@@ -28,7 +28,7 @@ func (a *AccountWallet) RequestBlockBuiltData(ctx context.Context, clt models.Cl
 		return nil, nil, 0, ierrors.Wrap(err, "failed to get block issuance data")
 	}
 
-	congestionResp, err := clt.GetCongestion(ctx, account.Address(), lo.PanicOnErr(issuerResp.Commitment.ID()))
+	congestionResp, err := clt.GetCongestion(ctx, account.Address(), lo.PanicOnErr(issuerResp.LatestCommitment.ID()))
 	if err != nil {
 		return nil, nil, 0, ierrors.Wrapf(err, "failed to get congestion data for issuer %s", account.Address())
 	}

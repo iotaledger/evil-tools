e5728b6e5469a96384167da8ecb1325552a64552
Daria Dziubałtowska <daria305@gmail.com>
2024-01-11 09:25:25 +0100

    Update test scripts with destroy command

--- scripts/test_account_implicit.sh
+++ scripts/test_account_implicit.sh
@@ -2,6 +2,11 @@ rm -f evil-tools
 rm -f *.dat
 go build
 
-./evil-tools accounts create --eviltools.accounts.create.alias IMPLICIT --eviltools.accounts.create.implicit
+./evil-tools accounts create --eviltools.accounts.create.alias IMPLICIT --eviltools.accounts.create.implicit --eviltools.accounts.create.transition
 
+./evil-tools spammer --eviltools.spammer.type blk --eviltools.spammer.rate 1 --eviltools.spammer.duration 10s --eviltools.spammer.account IMPLICIT
+
+./evil-tools accounts destroy --eviltools.accounts.destroy.alias IMPLICIT
+
+# this spam should not work, as account is now destroyed
 ./evil-tools spammer --eviltools.spammer.type blk --eviltools.spammer.rate 1 --eviltools.spammer.duration 10s --eviltools.spammer.account IMPLICIT
